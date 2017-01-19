1436f2f3465dc27495b4feacb8e4a8ff89ce03be
rsarkar15 <reena.sarkar15@gmail.com>
2017-01-19 14:52:18 -0500

    changes made during class

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 
 import os
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, send_file
 from db import setup_db, get_db
-#from memer import make_meme
+from memer import make_meme
+
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = bool(os.environ.get('FLASK_DEBUG', 1))
@@ -12,10 +14,14 @@ setup_db(app)
 
 @app.route('/add_meme', methods=['POST'])
 def add_meme():
-    get_db().execute('INSERT INTO memes(url, caption1, caption2) VALUES(?, ?, ?);', (
-        request.form['image'],
-        request.form['top_caption'],
-        request.form['bottom_caption']
+    bg_url = request.form['image']
+    top_caption = request.form['top_caption']
+    bottom_caption = request.form['bottom_caption']
+    get_db().execute('INSERT INTO memes(url, caption1, caption2, image) VALUES(?, ?, ?, ?);', (
+        bg_url,
+        top_caption,
+        bottom_caption,
+        make_meme(meme['url'], meme['caption1'], meme['caption2']).getvalue()
     ))
 
     return redirect(url_for('index'))
